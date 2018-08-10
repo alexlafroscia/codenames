2ad9447b56786d56384befa05d2048b641e5c2e7
Alex LaFroscia <alex@lafroscia.com>
2018-08-10 16:17:00 -0700

    chore: add Prettier formatting

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { RouteComponentProps } from "@reach/router";
 
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
 export const FETCH_GAME = gql`
   query FetchGame($id: String!) {
     game(id: $id) {
-      id 
+      id
     }
   }
-`
+`;
 
 interface GamePageProps {
   id: string;
@@ -20,15 +20,13 @@ export default class GamePage extends React.Component<GamePageProps> {
   public render() {
     const { id } = this.props;
 
-    return <>
-      <Query query={FETCH_GAME} variables={{ id }}>
-        {({ loading, data }) => (
-          loading
-          ? null
-          : data.game.id
-        )}
-      </Query>
-    </>;
+    return (
+      <>
+        <Query query={FETCH_GAME} variables={{ id }}>
+          {({ loading, data }) => (loading ? null : data.game.id)}
+        </Query>
+      </>
+    );
   }
 }
 
